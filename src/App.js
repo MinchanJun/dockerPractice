a314a06b881f82a7116f69971893dcf1980b9c24
MinchanJun <minchan@Minchans-MacBook-Pro.local>
2020-08-28 18:50:06 -0400

    docker practice with travis ci and deployment process with AWS

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React!!!!!!!!!!!!!!fwefew
         </a>
       </header>
     </div>
